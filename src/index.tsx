5cf0ea259914f2798cc7003585cdf4b3e57660be
Artur <semenukartur1@gmail.com>
2022-12-20 21:51:25 +0200

    ramuv div

--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,46 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
+const Title = () => {
+    return <h1>Hello world</h1>
+}
+
+const Content = () => {
+    return (
+        <React.Fragment>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum
+                fugiat, cupiditate iure est nulla quaerat nobis voluptatibus
+                blanditiis reprehenderit, corporis suscipit unde minus
+                laboriosam inventore sunt voluptate, dolor aliquam perspiciatis?
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum
+                fugiat, cupiditate iure est nulla quaerat nobis voluptatibus
+                blanditiis reprehenderit, corporis suscipit unde minus
+                laboriosam inventore sunt voluptate, dolor aliquam perspiciatis?
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum
+                fugiat, cupiditate iure est nulla quaerat nobis voluptatibus
+                blanditiis reprehenderit, corporis suscipit unde minus
+                laboriosam inventore sunt voluptate, dolor aliquam perspiciatis?
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum
+                fugiat, cupiditate iure est nulla quaerat nobis voluptatibus
+                blanditiis reprehenderit, corporis suscipit unde minus
+                laboriosam inventore sunt voluptate, dolor aliquam perspiciatis?
+            </p>
+        </React.Fragment>
+    )
+}
 const App = () => {
     return (
-        <div>
-            <h1>Hello React</h1>
-            <p>
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quos
-                aut incidunt ipsam eveniet rerum. Earum reiciendis aliquid
-                dolorum accusamus porro, illum quaerat reprehenderit rerum
-                asperiores. Quos distinctio tempora laudantium dolore?
-            </p>
-            <p>
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quos
-                aut incidunt ipsam eveniet rerum. Earum reiciendis aliquid
-                dolorum accusamus porro, illum quaerat reprehenderit rerum
-                asperiores. Quos distinctio tempora laudantium dolore?
-            </p>
-        </div>
+        <>
+            <Title />
+            <Content />
+        </>
     )
 }
 
